d412bb31fc4b3315397a974e57651767b5d96872
Ssanbas <94472180+Ssanbas@users.noreply.github.com>
2026-01-20 18:08:56 +0100

    PJ + Script

---
+++ FuegoElectro_Project/Assets/_FuegoElectro_ROOT/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    //Variables de referencia 
+
+    //Variables de estadística del player
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
