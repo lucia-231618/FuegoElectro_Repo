4521ed8a5a21074cba4baf931e623f448c01c751
Ssanbas <94472180+Ssanbas@users.noreply.github.com>
2026-01-21 21:38:12 +0100

    Script + Animator + Colliders (PLAYER)

--- FuegoElectro_Project/Assets/_FuegoElectro_ROOT/Scripts/PlayerController.cs
+++ FuegoElectro_Project/Assets/_FuegoElectro_ROOT/Scripts/PlayerController.cs
@@ -1,23 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{ 
-    //Variables de referencia 
+{
+    //Variables de referencia
+    private Rigidbody2D playerRb;
+    private Animator anim; 
+    private float horizontalInput;
 
-    //Variables de estadística del player
+    //Variables de estadÃ­stica del player
+    public float speed;
+    public float jumpForce;
+    [SerializeField] bool isGrounded;
+    [SerializeField] GameObject groundCheck;
+    [SerializeField] LayerMask groundLayer;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(
+    groundCheck.transform.position,
+    0.1f,
+    groundLayer
+);
+
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        if (Keyboard.current == null) return;
+
+        horizontalInput =
+            (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed ? -1 : 0) +
+            (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed ? 1 : 0);
+
+        playerRb.linearVelocity = new Vector2(
+            horizontalInput * speed,
+            playerRb.linearVelocity.y
+        );
+    }
+
+    void Jump()
+    {
+        if ((Keyboard.current.spaceKey.wasPressedThisFrame) && isGrounded)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
+
