a061e81bae93817f80fa76c09181408004dd4165
Ssanbas <94472180+Ssanbas@users.noreply.github.com>
2026-01-22 10:38:56 +0100

    PlayerAnim + LavaTileset

--- FuegoElectro_Project/Assets/_FuegoElectro_ROOT/Scripts/PlayerController.cs
+++ FuegoElectro_Project/Assets/_FuegoElectro_ROOT/Scripts/PlayerController.cs
@@ -5,12 +5,13 @@ public class PlayerController : MonoBehaviour
 {
     //Variables de referencia
     private Rigidbody2D playerRb;
-    private Animator anim; 
+    private Animator anim;
     private float horizontalInput;
 
     //Variables de estadística del player
     public float speed;
     public float jumpForce;
+    private bool isFacingRight = true;
     [SerializeField] bool isGrounded;
     [SerializeField] GameObject groundCheck;
     [SerializeField] LayerMask groundLayer;
@@ -18,15 +19,12 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(
-    groundCheck.transform.position,
-    0.1f,
-    groundLayer
-);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, 0.1f, groundLayer);
 
         Movement();
         Jump();
@@ -40,18 +38,47 @@ public class PlayerController : MonoBehaviour
             (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed ? -1 : 0) +
             (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed ? 1 : 0);
 
-        playerRb.linearVelocity = new Vector2(
-            horizontalInput * speed,
-            playerRb.linearVelocity.y
-        );
+        playerRb.linearVelocity = new Vector2(horizontalInput * speed,playerRb.linearVelocity.y);
+
+        //Flip: si el valor del input es diferente a 0
+        if (horizontalInput > 0) 
+        {
+            anim.SetBool("Running", true);
+            if (!isFacingRight)
+            { 
+                Flip();
+            }
+        }
+        if (horizontalInput < 0)
+        {
+            anim.SetBool("Running", true);
+            if (isFacingRight)
+            {
+                Flip();
+            }
+        }
+        if(horizontalInput == 0)
+        {
+            anim.SetBool("Running", false);
+        }
     }
 
     void Jump()
     {
+        anim.SetBool("Jumping", !isGrounded);
         if ((Keyboard.current.spaceKey.wasPressedThisFrame) && isGrounded)
         {
             playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+    }
+
+    void Flip()
+    { 
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1; 
+        transform.localScale = currentScale;
+        isFacingRight = !isFacingRight;
+    
     }
 }
 
